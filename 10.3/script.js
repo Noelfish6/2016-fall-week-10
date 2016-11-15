e0c2895cf63f6da66713d662bd61096b81a5e072
Noelfish6 <noel.fish6@qq.com>
2016-11-14 22:11:48 -0500

    Update script.js

--- 10.3/script.js
+++ 10.3/script.js
@@ -25,10 +25,44 @@ d3.queue()
     .defer(d3.json, '../data/gz_2010_us_050_00_5m.json')
     .defer(d3.tsv, '../data/unemployment.tsv', parseData)
     .await(function(err, geo, data){
+        console.log(geo);
+        console.table(data);
+        console.log(rate);
+
+        projection.fitExtent([[0,0],[w,h]],geo);
+
+        var counties = plot.selectAll('.county')
+            .data(geo.features)
+            .enter()
+            .append('path')
+            .attr('d',path)
+            .style('fill',function(d){
+                //Combine state and county id so we can look up unemployment rate
+                var id = (+d.properties.STATE) + d.properties.COUNTY;
+                var r = rate.get(id);
+                return scaleColor(r);
+            });
+
+        //Tooltip
+        counties.on('mouseenter',function(d){
+
+            })
+            .on('mousemove',function(d){
+
+            })
+            .on('mouseleave',function(d){
+
+            });
 
     });
 
 
 function parseData(d){
 
+    rate.set(d.id, +d.rate);
+
+    return {
+        id: d.id,
+        rate:+d.rate
+    }
 }
